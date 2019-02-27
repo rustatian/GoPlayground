a2fed86a4d9b90eb7c6f1811acacf33e6d37578a
Valery Piashchynski <piashchynski_valery@hotmail.com>
2019-02-27 18:13:41 +0300

    Can someone review this commit, please ?

---
+++ defer/initialization_trick/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type message struct {
+	content string
+}
+
+func (p *message) set(c string) {
+	p.content = c
+}
+func (p *message) print() string {
+	return p.content
+}
+
+func main() {
+	m := &message{content: "Hello"}
+	defer func() { fmt.Print(m.print()) }()
+	m.set("World")
+}
