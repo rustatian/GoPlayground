4f036e693528f127df71a6ce8bfc72bca9a20d34
Valery Piashchynski <piashchynski_valery@hotmail.com>
2019-05-17 17:28:01 +0300

    fix some fucking errors

--- lowlevelfun/memory/main.go
+++ lowlevelfun/memory/main.go
@@ -1,11 +1,18 @@
 package main
 
-import (
-	"runtime"
-)
+import "runtime"
 
 func main() {
-	a := make([]*int, 100)
+	//f, err := elf.Open("/Users/0xdev/Projects/repo/GoPlayground/lowlevelfun/memory/main")
+	//
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//for _, section := range f.Sections {
+	//	log.Println(section)
+	//}
+	a := make([]int, 100)
 	runtime.GC()
 	runtime.KeepAlive(a)
 }
