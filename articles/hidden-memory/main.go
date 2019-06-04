ce96c8eae1923bdf7efe9a11c4c41b99f57081ea
Valery Piashchynski <piashchynski_valery@hotmail.com>
2019-06-04 08:55:36 +0300

    ...

--- articles/hidden-memory/main.go
+++ articles/hidden-memory/main.go
@@ -1,14 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+var sliceLen int = 10000000
+
 type foo struct {
 	pSlice []*int
-	vSlice []int
+	//vSlice []int
 }
 
 func main() {
 	f := &foo{
-		pSlice: make([]*int, 1e8),
-		vSlice: make([]int, 1e8),
+		pSlice: make([]*int, sliceLen),
+		//vSlice: make([]int, sliceLen),
 	}
+
+	runtime.KeepAlive(f)
+
+
+	for i := 0; i <= 10; i++ {
+		start := time.Now()
+		runtime.GC()
+
+		fmt.Printf("GC time %s\n", time.Since(start))
+	}
+
 
 }
