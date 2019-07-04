686e2aa6224e9cd7fe710ea6f4afb99b6e247609
Valery Piashchynski <piashchynski_valery@hotmail.com>
2019-07-04 15:06:31 +0300

    This bug has driven lots of coders completely mad. You won't believe how it ended up being fixed

---
+++ stack/nosplit/main.go
@@ -0,0 +1,27 @@
+package main
+
+type T [1024000000]byte
+
+//go:nosplit
+func A(t T) {
+	B(t)
+}
+
+//go:nosplit
+func B(t T) {
+	C(t)
+}
+
+//go:nosplit
+func C(t T) {
+	D(t)
+}
+
+//go:noinline
+//go:nosplit
+func D(t T) {}
+
+func main() {
+	var t T
+	A(t)
+}
