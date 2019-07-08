2ffca84cafa7c8099f9ec600163b084b55cf35a3
Valery Piashchynski <piashchynski_valery@hotmail.com>
2019-07-08 19:13:08 +0300

    Just stop reading these for a while, ok..

--- slice/slice2.go
+++ slice/slice2.go
@@ -1,13 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
-	a := make([]byte, 0)
+	a := make([]byte, 10000)
+	wg := &sync.WaitGroup{}
+	wg.Add(100)
 
-	for i := 0; i < 10; i++ {
-		a = append(a, byte(i))
+	for i := 0; i <= 50; i++ {
+		go writeData(a, wg)
+		go writeData2(a, wg)
 	}
 
+	wg.Wait()
+
 	fmt.Print(a)
+}
+
+func writeData(data []byte, wg *sync.WaitGroup) {
+	defer wg.Done()
+	data[1] = byte('1')
+}
+
+func writeData2(data []byte, wg *sync.WaitGroup) {
+	defer wg.Done()
+	data[2] = byte('2')
+}
+
+func DoTheWork(data []interface{}, numGoroutines int) {
+
 }
