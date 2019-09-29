cccc1a69e67f775f4ae171a962ba08a8f574c113
Valery Piashchynski <piashchynski_valery@hotmail.com>
2019-09-29 19:23:02 +0300

    It worked for me...

--- http/main.go
+++ http/main.go
@@ -9,11 +9,11 @@ import (
 
 // request handler in fasthttp style, i.e. just plain function.
 func fastHTTPHandler(ctx *fasthttp.RequestCtx) {
-	fmt.Fprintf(ctx, "Hello")
+	_, _ = fmt.Fprintf(ctx, "Hello")
 }
 
 func main()  {
 	// pass plain function to fasthttp
-	fasthttp.ListenAndServe(":8081", fastHTTPHandler)
+	_ = fasthttp.ListenAndServe(":7878", fastHTTPHandler)
 }
 
