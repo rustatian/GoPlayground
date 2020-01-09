1f5b0fc711bdf8f53d3d0c7e90c2edc932328b7c
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-01-09 19:53:01 +0300

    s/    /  /g

--- temp/main.go
+++ temp/main.go
@@ -1,18 +1,14 @@
 package main
 
-import "fmt"
-
-func SuperPuperTestFuction(name string) {
-	fmt.Print(name)
-}
 
 func main() {
-	c := 3
-	d := 5
-	a:= make([]int, 0, c-d)
+	a:= make([]int, 10)
 	a = append(a, 1)
 	a = append(a, 2)
 	a = append(a, 3)
-	print(a)
+	for i := range a {
+		aa := &a[i]
+		println(*aa)
+	}
 }
 
