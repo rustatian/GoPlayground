c8cfdcdea2b7625e17524ea1bd2f773ea4289a13
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-01-17 00:31:32 +0300

    yet another quality commit

--- funQuestions/terminateOrNot.go
+++ funQuestions/terminateOrNot.go
@@ -1,11 +1,28 @@
 package main
 
-func main() {
-	for {
-		defer func() {
-			for{}
-		}()
-		panic("yolo")
-	}
 
+func main() {
+	data := make([]int, 5)
+	data[0] = 7
+	data[1] = 8
+	data[2] = 9
+	data[3] = 10
+	data[4] = 11
+	println(&data)
+	bts := data
+	println(&bts)
+	data = changeData(data)
+	println(&bts)
+	println(&data)
 }
+
+func changeData(data []int) []int {
+	data = changeData2(data)
+	return data
+}
+
+func changeData2(data []int) []int {
+	data = data[:1]
+	return data
+}
+
