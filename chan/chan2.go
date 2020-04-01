11579a07b7c0edad63cd93ee1999544fe1144540
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-04-01 22:40:36 +0300

    By works, I meant 'doesnt work'.  Works now..

--- chan/chan2.go
+++ chan/chan2.go
@@ -1,20 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"sync"
 )
 
 func main() {
-	c := make(chan int, 100)
+	a := 5
+	b := 6
+	c := 7
+	m := make(map[string]*int, 10)
+	m["1"] = &a
+	m["2"] = &b
+	m["3"] = &c
 	wg := &sync.WaitGroup{}
 
-	foo(c, wg)
+	for _, n := range m {
+		nn := n
+		wg.Add(1)
+		go func() {
+			fmt.Println(nn)
+			wg.Done()
+		}()
+	}
 
-
-
+	wg.Wait()
 }
 
-func foo(c chan int, wg *sync.WaitGroup) {
-
-}
 
--- temp/main.go
+++ temp/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "net/http"
-
 func main() {
-	http.ListenAndServe("localhost:2112", nil)
+	u := []uint8{123, 34, 94, 94, 92, 117, 48, 48, 50, 54, 48, 50, 94, 49, 94, 50, 94, 50, 53, 92, 117, 48, 48, 50, 54, 48, 51, 94, 49, 94, 50, 94, 51, 92, 117, 48, 48, 50, 54, 48, 51, 94, 49, 94, 49, 94, 49, 48, 48, 48, 48, 34, 58, 49, 125}
+
+	println(B2S(u))
+}
+
+func B2S(bs []uint8) string {
+	b := make([]byte, len(bs))
+	for i, v := range bs {
+		b[i] = byte(v)
+	}
+	return string(b)
 }
