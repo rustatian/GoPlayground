5585982f46812c49700c65fa5a71a0e2915d25e7
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-06-14 00:07:37 +0300

    tunning

--- reflection/function_call.go
+++ reflection/function_call.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"errors"
 	"reflect"
 	"unsafe"
 )
 
 type S1 struct {
-
 }
 
 func (s *S1) Init() error {
@@ -26,9 +26,37 @@ func funcAddr(fn interface{}) uintptr {
 
 func main() {
 	//s := *S1{}
-	v := reflect.Value{}.Set
+	reflect.MakeFunc()
+	v := reflect.Type(1)
 
 	addr := funcAddr((*S1).Init)
 	v.Pointer = addr
 	println(addr)
+}
+
+func CreateFunc(fType reflect.Type, f func(args []reflect.Value) (results []reflect.Value)) (reflect.Value, error) {
+	if fType.Kind() != reflect.Func {
+		return reflect.Value{}, errors.New("invalid input")
+	}
+
+	var ins, outs *[]reflect.Type
+
+	ins = new([]reflect.Type)
+	outs = new([]reflect.Type)
+
+	for i := 0; i < fType.NumIn(); i++ {
+		*ins = append(*ins, fType.In(i))
+	}
+
+	for i := 0; i < fType.NumOut(); i++ {
+		*outs = append(*outs, fType.Out(i))
+	}
+	var variadic bool
+	variadic = fType.IsVariadic()
+	return AllocateStackFrame(*ins, *outs, variadic, f), nil
+}
+func AllocateStackFrame(ins []reflect.Type, outs []reflect.Type, variadic bool, f func(args []reflect.Value) (results []reflect.Value)) reflect.Value {
+	var funcType reflect.Type
+	funcType = reflect.FuncOf(ins, outs, variadic)
+	return reflect.MakeFunc(funcType, f)
 }
