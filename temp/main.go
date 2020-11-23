3f36ca769ccf0195e795bd8a0973ab4aa2c6e65c
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-11-23 11:32:22 +0300

    better code

--- temp/main.go
+++ temp/main.go
@@ -1,25 +1,42 @@
 package main
 
-type ExchangeType string
-
-const (
-	Direct  ExchangeType = "direct"
-	Fanout  ExchangeType = "fanout"
-	Topic   ExchangeType = "topic"
-	Headers ExchangeType = "headers"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
 )
 
 func main() {
-	//isValidExchangeType := map[string]bool{"direct": true, "fanout": true, "topic": true, "headers": true}
 
-	foo()
+	url := "https://api.github.com/repos/octocat/hello-world/branches"
+	method := "GET"
 
-}
+	payload := strings.NewReader(` `)
 
-func foo(e ExchangeType) {
+	client := &http.Client {
+	}
+	req, err := http.NewRequest(method, url, payload)
 
-}
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	req.Header.Add("Accept", "application/vnd.github.v3+json")
+	req.Header.Add("Content-Type", "text/plain")
 
-type queue struct {
-	exchType ExchangeType
+	fmt.Println(req)
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(body))
 }
