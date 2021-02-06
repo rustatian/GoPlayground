e61e58e0416cf2184406df4b21f8739b7a9f7c23
Valery Piashchynski <piashchynski.valery@gmail.com>
2021-02-07 01:30:00 +0300

    We're no strangers to love

--- secret/transport/ws.go
+++ secret/transport/ws.go
@@ -1,8 +1,6 @@
 package transport
 
 import (
-	"fmt"
-
 	"github.com/48d90782/GoPlayground/secret/pkg/shared_data"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/websocket/v2"
@@ -15,6 +13,7 @@ type Websocket struct {
 
 // TODO logger
 func NewWSHandler(app *fiber.App, log *zap.Logger, shared shared_data.Shared) error {
+	// websocket middleware
 	app.Use("/ws", func(c *fiber.Ctx) error {
 		// IsWebSocketUpgrade returns true if the client
 		// requested upgrade to the WebSocket protocol.
@@ -27,10 +26,10 @@ func NewWSHandler(app *fiber.App, log *zap.Logger, shared shared_data.Shared) er
 
 	app.Get("/ws/:id", websocket.New(func(c *websocket.Conn) {
 		// c.Locals is added to the *websocket.Conn
-		fmt.Println(c.Locals("allowed"))  // true
-		fmt.Println(c.Params("id"))       // 123
-		fmt.Println(c.Query("v"))         // 1.0
-		fmt.Println(c.Cookies("session")) // ""
+		log.Debug("allowed", zap.Bool("allowed", c.Locals("allowed").(bool)))
+		log.Debug("locals", zap.String("params", c.Params("id")))
+		log.Debug("query", zap.String("query", c.Query("v")))
+		log.Debug("session", zap.String("session", c.Cookies("session")))
 
 		// websocket.Conn bindings https://pkg.go.dev/github.com/fasthttp/websocket?tab=doc#pkg-index
 		var (
@@ -41,13 +40,13 @@ func NewWSHandler(app *fiber.App, log *zap.Logger, shared shared_data.Shared) er
 		// for every connection
 		for {
 			if mt, msg, err = c.ReadMessage(); err != nil {
-				fmt.Println("read:", err)
+				log.Debug("read:", zap.Error(err))
 				break
 			}
-			fmt.Printf("recv: %s", msg)
+			log.Debug("receive", zap.String("recv", string(msg)))
 
 			if err = c.WriteMessage(mt, msg); err != nil {
-				fmt.Println("write:", err)
+				log.Debug("write:", zap.Error(err))
 				break
 			}
 		}
