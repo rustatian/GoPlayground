df08142df09085d21c16dac83e118e4f66f90eb3
Valery Piashchynski <piashchynski.valery@gmail.com>
2021-04-16 14:25:11 +0300

    yo recipes

---
+++ strings/main.go
@@ -0,0 +1,18 @@
+package main
+
+func main() {
+	a := "954fd71c-8f91-44f8-b63c-36d4f120dfcd"
+	b := "954fd71c-8f91-44f8-b63c-36d4f120dfdc"
+
+	if a == b {
+		println("a == b")
+	}
+
+	if a < b {
+		println("a < b")
+	}
+
+	if a > b {
+		println("a > b")
+	}
+}
