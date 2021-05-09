ea077086c40a6c632acbafb4e1d5dad854459265
Valery Piashchynski <piashchynski.valery@gmail.com>
2021-05-09 17:18:32 +0300

    You know the rules and so do I

--- test/main.go
+++ test/main.go
@@ -1,30 +1,12 @@
 package main
 
-import (
-	"sync"
-	"sync/atomic"
-)
-
 func main() {
 
 }
 
-type Shared struct {
-	sync.RWMutex
-	data      uint64
-	lockValue uint64
-}
-
 //go:noinline
-func (s *Shared) test_mutexes() {
-	s.Lock()
-	s.data = 132
-	s.Unlock()
-}
-
-//go:noinline
-func (s *Shared) test_atomic() {
-	if atomic.CompareAndSwapUint64(&s.lockValue, 0, 0) {
-		s.data = 132
+func test_btb(bp bool) {
+	if bp {
+		println("hello, I'm function")
 	}
 }
--- test/main_test.go
+++ test/main_test.go
@@ -2,28 +2,20 @@ package main
 
 import (
 	"testing"
+	"time"
 )
 
-func BenchmarkMutex(b *testing.B) {
-	s := &Shared{
-		data:      0,
-		lockValue: 0,
-	}
-	b.ReportAllocs()
-	b.ResetTimer()
-	for n := 0; n < b.N; n++ {
-		s.test_mutexes()
-	}
-}
+func TestAtomic(t *testing.T) {
+	tt := time.Now()
+	test_btb(true)
+	println(time.Now().Sub(tt).Nanoseconds())
 
-func BenchmarkAtomic(b *testing.B) {
-	s := &Shared{
-		data:      0,
-		lockValue: 0,
-	}
-	b.ReportAllocs()
-	b.ResetTimer()
-	for n := 0; n < b.N; n++ {
-		s.test_atomic()
-	}
+
+	tt2 := time.Now()
+	test_btb(true)
+	println(time.Now().Sub(tt2).Nanoseconds())
+
+	tt3 := time.Now()
+	test_btb(true)
+	println(time.Now().Sub(tt3).Nanoseconds())
 }
