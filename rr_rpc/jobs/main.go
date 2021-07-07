a5963ed197322f3c32159e3818591915ddfd7ef7
Valery Piashchynski <piashchynski.valery@gmail.com>
2021-07-07 18:14:55 +0300

    At times like this I wish I was a Garbage Man.

--- rr_rpc/jobs/main.go
+++ rr_rpc/jobs/main.go
@@ -16,23 +16,136 @@ var rate = flag.Uint64("r", 1, "filters rate per second")
 func main() {
 	flag.Parse()
 
-	payload := &jobsv1beta.Request{
-		Job: &jobsv1beta.Job{
-			Job:     "test",
-			Payload: "helllllllllllllllllllloooooooooooooooooooo",
-			Options: &jobsv1beta.Options{
-				Priority:   100,
-				Pipeline:   "test-local",
-				Delay:      0,
-				Attempts:   0,
-				RetryDelay: 0,
-				Timeout:    0,
+	//payload := &jobsv1beta.Request{
+	//	Job: &jobsv1beta.Job{
+	//		Job:     "test",
+	//		Payload: "helllllllllllllllllllloooooooooooooooooooo",
+	//		Options: &jobsv1beta.Options{
+	//			Priority:   100,
+	//			Pipeline:   "test-local",
+	//			Delay:      0,
+	//			Attempts:   0,
+	//			RetryDelay: 0,
+	//			Timeout:    0,
+	//		},
+	//	},
+	//}
+
+	payloads := &jobsv1beta.PushBatchRequest{
+		Jobs: []*jobsv1beta.Job{
+			{
+				Job:     "test1",
+				Id:      "1",
+				Payload: "test1-fffffffffffffff",
+				Options: &jobsv1beta.Options{
+					Priority: 11,
+					Pipeline: "test-local",
+				},
+			},
+			{
+				Job:     "test2",
+				Id:      "1",
+				Payload: "test2-ffffffffffffffff",
+				Options: &jobsv1beta.Options{
+					Priority: 10,
+					Pipeline: "test-local",
+				},
+			},
+			{
+				Job:     "test3",
+				Id:      "1",
+				Payload: "test3-ffffffffffffffff",
+				Options: &jobsv1beta.Options{
+					Priority: 12,
+					Pipeline: "test-local",
+				},
+			},
+			{
+				Job:     "test1",
+				Id:      "1",
+				Payload: "test1-fffffffffffffff",
+				Options: &jobsv1beta.Options{
+					Priority: 11,
+					Pipeline: "test-local",
+				},
+			},
+			{
+				Job:     "test2",
+				Id:      "1",
+				Payload: "test2-ffffffffffffffff",
+				Options: &jobsv1beta.Options{
+					Priority: 10,
+					Pipeline: "test-local",
+				},
+			},
+			{
+				Job:     "test3",
+				Id:      "1",
+				Payload: "test3-ffffffffffffffff",
+				Options: &jobsv1beta.Options{
+					Priority: 12,
+					Pipeline: "test-local",
+				},
+			},
+			{
+				Job:     "test1",
+				Id:      "1",
+				Payload: "test1-fffffffffffffff",
+				Options: &jobsv1beta.Options{
+					Priority: 11,
+					Pipeline: "test-local",
+				},
+			},
+			{
+				Job:     "test2",
+				Id:      "1",
+				Payload: "test2-ffffffffffffffff",
+				Options: &jobsv1beta.Options{
+					Priority: 10,
+					Pipeline: "test-local",
+				},
+			},
+			{
+				Job:     "test3",
+				Id:      "1",
+				Payload: "test3-ffffffffffffffff",
+				Options: &jobsv1beta.Options{
+					Priority: 12,
+					Pipeline: "test-local",
+				},
+			},
+			{
+				Job:     "test1",
+				Id:      "1",
+				Payload: "test1-fffffffffffffff",
+				Options: &jobsv1beta.Options{
+					Priority: 11,
+					Pipeline: "test-local",
+				},
+			},
+			{
+				Job:     "test2",
+				Id:      "1",
+				Payload: "test2-ffffffffffffffff",
+				Options: &jobsv1beta.Options{
+					Priority: 10,
+					Pipeline: "test-local",
+				},
+			},
+			{
+				Job:     "test3",
+				Id:      "1",
+				Payload: "test3-ffffffffffffffff",
+				Options: &jobsv1beta.Options{
+					Priority: 12,
+					Pipeline: "test-local",
+				},
 			},
 		},
 	}
 
 	ch := make(chan struct{})
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < 20; i++ {
 		go func() {
 			conn, err := net.Dial("unix", *addr)
 			if err != nil {
@@ -41,9 +154,9 @@ func main() {
 
 			client := rpc.NewClientWithCodec(goridgeRpc.NewClientCodec(conn))
 
-			for j := 0; j < 20000000; j++ {
-				resp := jobsv1beta.Response{}
-				err = client.Call("jobs.Push", payload, &resp)
+			for j := 0; j < 10000; j++ {
+				resp := jobsv1beta.EmptyResponse{}
+				err = client.Call("jobs.PushBatch", payloads, &resp)
 				if err != nil {
 					panic(err)
 				}
