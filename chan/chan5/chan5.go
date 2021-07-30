20118c83d4e563f2e514e41c50352c3e1a3bbcbe
Valery Piashchynski <piashchynski.valery@gmail.com>
2021-07-30 19:26:12 +0300

    Fixed some shit

---
+++ chan/chan5/chan5.go
@@ -0,0 +1,18 @@
+package main
+
+
+func main() {
+	ch := make(chan int, 10)
+
+	for i := 0; i < 10; i++ {
+		ch <- i
+	}
+
+	for i := 0; i < 10; i++ {
+		j := <-ch
+
+		println(j)
+
+		ch <- j
+	}
+}
