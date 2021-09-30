cad33d7f329768d0d23875c21ec6878c36cb00b2
Valery Piashchynski <piashchynski.valery@gmail.com>
2021-09-30 23:37:54 +0300

    tagging release w.t.f.

--- acme/letsencrypt/main.go
+++ acme/letsencrypt/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
+	"log"
+	"net/http"
 
 	"github.com/caddyserver/certmagic"
+	"golang.org/x/sys/cpu"
 )
 
 func main() {
@@ -39,9 +43,9 @@ func main() {
 	})
 
 	myAcme := certmagic.NewACMEManager(cfg, certmagic.ACMEManager{
-		CA:                      certmagic.LetsEncryptStagingCA,
+		CA:                      certmagic.LetsEncryptProductionCA,
 		TestCA:                  certmagic.LetsEncryptStagingCA,
-		Email:                   "govnomulo@mail.ru",
+		Email:                   "piashchynski.valery@gmail.com",
 		Agreed:                  true,
 		DisableHTTPChallenge:    false,
 		DisableTLSALPNChallenge: false,
@@ -58,7 +62,7 @@ func main() {
 
 	cfg.Issuers = append(cfg.Issuers, myAcme)
 
-	err := cfg.ObtainCertAsync(context.Background(), "testtesttest.club")
+	err := cfg.ObtainCertAsync(context.Background(), "piashchynski.valery@gmail.com")
 	if err != nil {
 		panic(err)
 	}
@@ -67,4 +71,70 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	var topCipherSuites []uint16
+	var defaultCipherSuitesTLS13 []uint16
+
+	hasGCMAsmAMD64 := cpu.X86.HasAES && cpu.X86.HasPCLMULQDQ
+	hasGCMAsmARM64 := cpu.ARM64.HasAES && cpu.ARM64.HasPMULL
+	// Keep in sync with crypto/aes/cipher_s390x.go.
+	hasGCMAsmS390X := cpu.S390X.HasAES && cpu.S390X.HasAESCBC && cpu.S390X.HasAESCTR && (cpu.S390X.HasGHASH || cpu.S390X.HasAESGCM)
+
+	hasGCMAsm := hasGCMAsmAMD64 || hasGCMAsmARM64 || hasGCMAsmS390X
+
+	if hasGCMAsm {
+		// If AES-GCM hardware is provided then priorities AES-GCM
+		// cipher suites.
+		topCipherSuites = []uint16{
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+		}
+		defaultCipherSuitesTLS13 = []uint16{
+			tls.TLS_AES_128_GCM_SHA256,
+			tls.TLS_CHACHA20_POLY1305_SHA256,
+			tls.TLS_AES_256_GCM_SHA384,
+		}
+	} else {
+		// Without AES-GCM hardware, we put the ChaCha20-Poly1305
+		// cipher suites first.
+		topCipherSuites = []uint16{
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+		}
+		defaultCipherSuitesTLS13 = []uint16{
+			tls.TLS_CHACHA20_POLY1305_SHA256,
+			tls.TLS_AES_128_GCM_SHA256,
+			tls.TLS_AES_256_GCM_SHA384,
+		}
+	}
+
+	DefaultCipherSuites := make([]uint16, 0, 22)
+	DefaultCipherSuites = append(DefaultCipherSuites, topCipherSuites...)
+	DefaultCipherSuites = append(DefaultCipherSuites, defaultCipherSuitesTLS13...)
+
+	sslServer := &http.Server{
+		Addr: "0.0.0.0:443",
+		TLSConfig: &tls.Config{
+			CurvePreferences: []tls.CurveID{
+				tls.X25519,
+				tls.CurveP256,
+				tls.CurveP384,
+				tls.CurveP521,
+			},
+			CipherSuites:             DefaultCipherSuites,
+			MinVersion:               tls.VersionTLS12,
+			PreferServerCipherSuites: true,
+			GetCertificate:           cfg.GetCertificate,
+		},
+	}
+
+	log.Fatal(sslServer.ListenAndServeTLS("", ""))
 }
