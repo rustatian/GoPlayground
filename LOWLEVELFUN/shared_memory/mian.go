1ab1d334aa0e38a736e2e83e82010698fa21558f
Valery Piashchynski <piashchynski.valery@gmail.com>
2021-10-17 20:22:26 +0300

    Blaming regex.

---
+++ LOWLEVELFUN/shared_memory/mian.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/spiral/goridge/v3/pkg/shared_memory/posix"
+)
+
+func main() {
+	seg, err := posix.AttachToShmSegment(6815800, 1024, 0666)
+	if err != nil {
+		panic(err)
+	}
+
+	data := make([]byte, 1024)
+	err = seg.Read(data)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(data))
+}
