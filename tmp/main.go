656a6d6e3a9101cca9e129a7ae585418a1ee0426
Valery Piashchynski <piashchynski.valery@gmail.com>
2022-01-30 15:30:57 +0300

    Either Hot Shit or Total Bollocks

--- tmp/main.go
+++ tmp/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"io"
+	"net/http"
+)
 
+func main() {
+	resp, err := http.Get("http://example.com/")
+	if err != nil {
+		panic(err)
+	}
+	_, err = io.ReadAll(resp.Body)
+	_ = resp.Body.Close()
 }
