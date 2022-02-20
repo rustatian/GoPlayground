6942b997dcce57e81630e88c551379dc2dd65596
Valery Piashchynski <piashchynski.valery@gmail.com>
2022-02-20 08:53:57 +0300

    Who Let the Bugs Out??

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/go-acme/lego/v4 v4.4.0
 	github.com/golang/protobuf v1.5.2
 	github.com/google/flatbuffers v1.12.0
+	github.com/google/go-github/v42 v42.0.0
 	github.com/google/gopacket v1.1.17
 	github.com/mmcloughlin/avo v0.2.0
 	github.com/nats-io/nats.go v1.13.1-0.20220121202836-972a071d373d
@@ -24,24 +25,26 @@ require (
 )
 
 require (
+	cloud.google.com/go v0.93.3 // indirect
 	github.com/akutz/memconn v0.1.0 // indirect
 	github.com/andybalholm/brotli v1.0.3 // indirect
-	github.com/cenkalti/backoff/v4 v4.1.1 // indirect
 	github.com/goccy/go-json v0.9.4 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/googleapis/gax-go/v2 v2.1.0 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/klauspost/cpuid/v2 v2.0.6 // indirect
 	github.com/libdns/libdns v0.2.1 // indirect
 	github.com/mholt/acmez v1.0.0 // indirect
 	github.com/miekg/dns v1.1.43 // indirect
 	github.com/mitchellh/go-ps v1.0.0 // indirect
-	github.com/nats-io/nats-server/v2 v2.7.2 // indirect
 	github.com/nats-io/nkeys v0.3.0 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/vmihailenco/msgpack/v5 v5.3.5 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	go.opencensus.io v0.23.0 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
-	go.uber.org/multierr v1.7.0 // indirect
 	go4.org/intern v0.0.0-20211027215823-ae77deb06f29 // indirect
 	go4.org/mem v0.0.0-20210711025021-927187094b94 // indirect
 	go4.org/unsafe/assume-no-moving-gc v0.0.0-20211027215541-db492cf91b37 // indirect
@@ -50,7 +53,16 @@ require (
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/tools v0.1.8 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20210828152312-66f60bf46e71 // indirect
 	gopkg.in/square/go-jose.v2 v2.5.1 // indirect
 	inet.af/netaddr v0.0.0-20211027220019-c74959edd3b6 // indirect
+)
+
+require (
+	github.com/cenkalti/backoff/v4 v4.1.1 // indirect
+	github.com/nats-io/nats-server/v2 v2.7.2 // indirect
+	go.uber.org/multierr v1.7.0 // indirect
+	golang.org/x/oauth2 v0.0.0-20210819190943-2bc19b11175f
+	google.golang.org/api v0.56.0
 )
--- tmp/main.go
+++ tmp/main.go
@@ -1,15 +1,120 @@
 package main
 
 import (
-	"io"
+	"context"
+	"log"
 	"net/http"
+
+	"github.com/google/go-github/v42/github"
+	"golang.org/x/oauth2"
+	"google.golang.org/api/option"
+	"google.golang.org/api/sheets/v4"
 )
 
+type user struct {
+	IssueTitle        string
+	UserBio           string
+	Twitter           string
+	NumberOfFollowers int
+	Email             string
+	Username          string
+	LoginURL          string
+	Company           string
+	IssueLink         string
+}
+
 func main() {
-	resp, err := http.Get("http://example.com/")
-	if err != nil {
-		panic(err)
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: ""})
+	ghClient := github.NewClient(oauth2.NewClient(ctx, ts))
+
+	users := make([]*user, 0, 10)
+
+	for i := 1; i < 1009; i++ {
+		u := &user{}
+
+		log.Printf("issue: %d\n", i)
+		issue, resp, err := ghClient.Issues.Get(context.Background(), "", "", i)
+		if err != nil {
+			continue
+		}
+		if issue.IsPullRequest() {
+			continue
+		}
+
+		if resp.StatusCode != http.StatusOK {
+			log.Println("bad issue")
+			continue
+		}
+
+		if *issue.User.Login == "" || *issue.User.Login == "" || *issue.User.Login == "" {
+			log.Println("skipped")
+			continue
+		}
+
+		u.Username = *issue.User.Login
+		u.IssueTitle = *issue.Title
+
+		usr, resp2, err := ghClient.Users.Get(context.Background(), *issue.User.Login)
+		if err != nil {
+			continue
+		}
+
+		if resp2.StatusCode != http.StatusOK {
+			log.Println("bad usr")
+			continue
+		}
+
+		if usr.Company != nil {
+			u.Company = *usr.Company
+		} else {
+			u.Company = "no-company"
+		}
+
+		u.LoginURL = *usr.HTMLURL
+		u.IssueLink = *issue.HTMLURL
+
+		if usr.Bio != nil {
+			u.UserBio = *usr.Bio
+		}
+
+		if usr.Email != nil {
+			u.Email = *usr.Email
+		}
+
+		if usr.TwitterUsername != nil {
+			u.Twitter = *usr.TwitterUsername
+		}
+
+		if usr.Followers != nil {
+			u.NumberOfFollowers = *usr.Followers
+		}
+
+		users = append(users, u)
 	}
-	_, err = io.ReadAll(resp.Body)
-	_ = resp.Body.Close()
+
+	source := oauth2.StaticTokenSource(&oauth2.Token{TokenType: "Bearer", RefreshToken: "", AccessToken: ""})
+	client := oauth2.NewClient(context.Background(), source)
+
+	srv, err := sheets.NewService(ctx, option.WithHTTPClient(client))
+	if err != nil {
+		log.Fatalf("Unable to retrieve Sheets client: %v", err)
+	}
+
+	spreadsheetId := ""
+	writeRange := "A2:I10000"
+
+	var vr sheets.ValueRange
+	for i := 0; i < len(users); i++ {
+		tmpVal := []interface{}{users[i].Username, users[i].Company, users[i].UserBio, users[i].Email, users[i].NumberOfFollowers, users[i].Twitter, users[i].IssueLink, users[i].IssueTitle, users[i].LoginURL}
+		vr.Values = append(vr.Values, tmpVal)
+	}
+
+	updCall := srv.Spreadsheets.Values.Update(spreadsheetId, writeRange, &vr)
+	updCall.ValueInputOption("RAW")
+
+	_, err = updCall.Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve data from sheet: %v", err)
+	}
 }
